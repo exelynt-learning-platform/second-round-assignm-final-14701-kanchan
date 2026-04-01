ad475d37a0649a86a42b4d847ae16b5514b4d0b3
Kanchan Pawadre <75987758+mekanchan610@users.noreply.github.com>
2026-04-01 10:16:52 -1200

    Update Cart.java

--- Cart.java
+++ Cart.java
@@ -1,19 +1,21 @@
-
 package com.ecommerce.entity;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Data
 public class Cart {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne
     private User user;
 
     @ManyToMany
-    private List<Product> products;
+    private List<Product> products = new ArrayList<>(); // initialized to avoid NPE
 }
