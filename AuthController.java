cf1464cd333a347e35025f00fdc2cc04fc77a29c
Kanchan Pawadre <75987758+mekanchan610@users.noreply.github.com>
2026-04-01 09:19:25 -1200

    Update AuthController.java

--- AuthController.java
+++ AuthController.java
@@ -1,8 +1,8 @@
-
 package com.ecommerce.controller;
 
 import com.ecommerce.entity.User;
 import com.ecommerce.repository.UserRepository;
+import com.ecommerce.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
@@ -14,20 +14,28 @@ public class AuthController {
 
     private final UserRepository repo;
     private final PasswordEncoder encoder;
+    private final JwtUtil jwtUtil;
 
     @PostMapping("/register")
     public String register(@RequestBody User user) {
         user.setPassword(encoder.encode(user.getPassword()));
         repo.save(user);
-        return "User Registered";
+        return "User Registered Successfully";
     }
 
     @PostMapping("/login")
     public String login(@RequestBody User user) {
+
         User db = repo.findByUsername(user.getUsername());
+
         if (db != null && encoder.matches(user.getPassword(), db.getPassword())) {
-            return "Login Successful";
+
+            // ✅ Generate JWT token
+            String token = jwtUtil.generateToken(db.getUsername());
+
+            return token; // IMPORTANT
         }
-        return "Invalid Credentials";
+
+        throw new RuntimeException("Invalid Credentials");
     }
 }
