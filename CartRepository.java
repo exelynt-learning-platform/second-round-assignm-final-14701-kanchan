5fff3a7d91b84c1f3e0e63db040522bb5951854d
Kanchan Pawadre <75987758+mekanchan610@users.noreply.github.com>
2026-04-01 10:23:20 -1200

    Update CartRepository.java

--- CartRepository.java
+++ CartRepository.java
@@ -1,9 +1,11 @@
-
 package com.ecommerce.repository;
 
 import com.ecommerce.entity.Cart;
+import com.ecommerce.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
-    Cart findByUserId(Long userId);
+
+    Cart findByUserId(Long userId); // existing
+    Cart findByUser(User user);     // add this
 }
